b60336b3b1eb2a93a0eaf26e2d12133c34106ad5
Joey Schrader <jschrader@overstock.com>
2020-08-07 10:15:37 -0600

    created prod build

---
+++ client/build/precache-manifest.2d29a216b4dcaab5b5f679da8e148f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec3bcae0fb587816416f45f570930ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e508c9c18b10593880f",
+    "url": "/static/css/main.1e4ef2ed.chunk.css"
+  },
+  {
+    "revision": "6781be015d7f8dfdbca7",
+    "url": "/static/js/2.f49511ad.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.f49511ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e508c9c18b10593880f",
+    "url": "/static/js/main.a49415ed.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
