5dfea32d76626121eea9e6940b564d4734c486b9
Joey Schrader <jschrader@overstock.com>
2020-08-07 07:18:38 -0600

    removed client git repo

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('server running')
+});
